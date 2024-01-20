3e8b4d973fb07d17c98a76bf679c60a4b239ab7f
Ella Ferreira <rafaela.morato1805@gmail.com>
2024-01-20 22:04:14 +1000

    singup func - WIP

---
+++ components/PasswordValidationInput.tsx
@@ -0,0 +1,56 @@
+import Eye from "@/app/assets/icons/eye";
+import EyeSlash from "@/app/assets/icons/eye-slash";
+import { ChangeEvent, useState } from "react";
+
+export type PasswordValidationProps = {
+  handleChange: (e: ChangeEvent<HTMLInputElement>) => void;
+  handleBlur: (value: any) => void;
+  placeholder?: string;
+  error?: string;
+  touched?: boolean;
+  value: string;
+  className?: string;
+};
+
+function PasswordValidationInput({
+  placeholder,
+  error,
+  touched,
+  value,
+  className,
+  handleChange,
+  handleBlur,
+}: PasswordValidationProps) {
+  const [shouldShowPassword, setShouldShowPassword] = useState(false);
+
+  const tooglePasswordVisibility = () => {
+    setShouldShowPassword(!shouldShowPassword);
+  };
+
+  return (
+    <div className="relative w-full">
+      <input
+        className={`input-primary text-[16px] ` + className}
+        type={shouldShowPassword ? "text" : "password"}
+        placeholder={placeholder}
+        value={value}
+        onChange={(value) => handleChange(value)}
+        onBlur={(value) => handleBlur(value)}
+      />
+      <div
+        className="absolute right-2 top-[15px]"
+        onClick={tooglePasswordVisibility}
+      >
+        {shouldShowPassword ? <Eye /> : <EyeSlash />}
+      </div>
+
+      {error && touched && (
+        <div className="bg-error-base p-2 text-white-base border-[1px] border-grey-100 rounded-lg mb-4 text-[14px]">
+          {error}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default PasswordValidationInput;
