ec72ae48133b976dd2aa8e4b9a7aaf6067267b94
Tom McCarthy <tom@tomm.cc>
2024-01-21 07:00:23 +1000

    remove unused import

--- app/attendee/page.tsx
+++ app/attendee/page.tsx
@@ -1,8 +1,5 @@
-import Link from "next/link";
-import { headers, cookies } from "next/headers";
+import { cookies } from "next/headers";
 import { createClient } from "@/utils/supabase/server";
-import { redirect } from "next/navigation";
-import './attendee-page.css'
 import Image from 'next/image'
 import artistPhoto from './images/pexels-photo-167636.jpeg'
 import instagram from './images/instagram.png'
@@ -11,10 +8,10 @@ import facebook from './images/facebook.png'
 
 
 export default async function Login() {
- 
+
   const cookieStore = cookies();
   const supabase = createClient(cookieStore);
-  
+
   const { data, error } = await supabase
   .from('Users')
   .select()
@@ -48,7 +45,7 @@ export default async function Login() {
       }
     ]
   }
-  
+
   let eventData = {
     eventName: "Event Name",
     artistName: "Artist Name",
@@ -58,7 +55,7 @@ export default async function Login() {
 
     <>
       <div className="flex-1 flex flex-col w-full px-8 sm:max-w-md justify-center p-12 gap-8 blue-50">
-    
+
       {/* <h1>{data.username}</h1> */}
       <div>
       <Image
@@ -75,7 +72,7 @@ export default async function Login() {
         <h2 className="text-3xl font-bold">event name</h2>
       </div>
       <div className="button-wrapper flex flex-col gap-3 text-center">
-        { userData.buttonLinks.map(button => 
+        { userData.buttonLinks.map(button =>
           <a href="#" className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full" target="_blank">{button.text}
             <button >
             </button>
@@ -87,7 +84,7 @@ export default async function Login() {
         <h2 className="text-2xl font-bold">Socials</h2>
         <div className="flex flex-row flex-wrap gap-2 justify-start items-center">
           <a href="#">
-          <Image src={instagram} style={{width: 40,height:40}}  alt="instagram"  />  
+          <Image src={instagram} style={{width: 40,height:40}}  alt="instagram"  />
           </a>
           <a href="#">
           <Image src={youtube} style={{width: 50,height:50}}  alt="youtube"  />
